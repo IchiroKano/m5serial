e9100e91b76579d23b58350d64c3e9f5e6600828
I.KANO <ichiro.kano2@asahi.email.ne.jp>
2018-07-16 22:04:39 +0900

    update command
    add sonic sensor, reply date and time via UART serial communication

--- serialCom.py
+++ serialCom.py
@@ -1,10 +1,12 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import serial
 import threading
+import datetime
 import time
+import wiringpi as pi
+
 
 serCom = serial.Serial('/dev/ttyAMA0', 115200, timeout = 1 )
 
@@ -15,19 +17,34 @@ class serialRead():
         self.threadRead.start()
 
     def target(self):
-        while True:
+        flag=True
+        while flag:
             if( serCom.inWaiting() > 0 ):
                 strC = serCom.read( serCom.inWaiting() )
                 print( "[%s]" % strC )
-                if( strC == "IP addre"):
-                    serCom.write( "192.168.1.23" )
-                if( strC == "Date" ):
-                    serCom.write("2018 JULY 07")
-                if( strC == "Time"):
-                    serCom.write("Message from my PC")
+                if( strC == "Sonic"):
+                    num = 0
+                    while num < 10:
+                        num = num + 1
+                        strDistance = mesure()
+                        serCom.write( strDistance )
+                        print( strDistance )
+                        time.sleep(1.0)
 
-    def stop(self):
-        self.stop_event.set()
+                if( strC == "Date" ):
+                    objDate = datetime.date.today()
+                    strDate = objDate.strftime("%Y/%m/%d")
+                    serCom.write(strDate)
+                    print(strDate)
+
+                if( strC == "Time"):
+                    objTime = datetime.datetime.now()
+                    strTime = objTime.strftime("%H:%M:%S")
+                    serCom.write(strTime)
+                    print(strTime)
+
+    def quit(self):
+        flag=False
 
 def main():
     print( "Start reading..." , serCom.portstr )
@@ -37,10 +54,35 @@ def main():
             serCom.write( strA )
             print( "[%s] sent" % strA )
         except:
-            threadRead.stop()
             serCom.close()
+            threadRead.quit()
             exit()
 
+def sonicInit():
+    global TRIG_PIN
+    global ECHO_PIN
+    global pi
+    TRIG_PIN = 18
+    ECHO_PIN = 17
+    pi.wiringPiSetupGpio()
+    pi.pinMode( TRIG_PIN, pi.OUTPUT )
+    pi.pinMode( ECHO_PIN, pi.INPUT )
+    pi.digitalWrite( TRIG_PIN, pi.LOW )
+
+def mesure():
+    pi.digitalWrite( TRIG_PIN, pi.HIGH )
+    time.sleep(0.00001)
+    pi.digitalWrite( TRIG_PIN, pi.LOW )
+    while ( pi.digitalRead( ECHO_PIN ) == pi.LOW ):
+        sigoff = time.time()
+    while ( pi.digitalRead( ECHO_PIN ) == 1 ):
+        sigon = time.time()
+    strTemp = str( round((( sigon - sigoff ) * 34000) / 2, 5 )) + "cm"
+    time.sleep(1.0)
+    return ( strTemp )
+
 if __name__ == '__main__':
+    sonicInit()
     threadRead = serialRead()
     main()
+
